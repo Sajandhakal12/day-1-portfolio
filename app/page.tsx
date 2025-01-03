6dce20abd1ab9d15060dc1e340060cf050d395a5
sajandhakal12 <sajan12dhakal@gmail.com>
2025-01-03 11:54:02 +0545

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,121 @@
 import Image from "next/image";
+import { AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="px-[200px] py-2 ">
+      <header className="flex justify-between text-lg">
+        <h1 className="">Madelyn Torff</h1>
+        <nav className="flex gap-10">
+          <a href="">About</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+        </nav>
+      </header>
+      <main>
+        <div className="flex justify-between relative h-[80vh]">
+          <div className=" mt-[75px] w-[40%]">
+            <p className="uppercase mb-5 font-extrabold text-amber-400 text-xl">
+              UI/UX Designer
+            </p>
+            <h1 className="text-6xl mb-[24px] text-justify font-bold">
+              Hello, My name is Madelyn Torff
+            </h1>
+            <p className="text-slate-400 text-2xl font-light text-justify mb-[24px]">
+              Short text with details about you, what you do or your
+              professional career. You can add more information on the about
+              page.
+            </p>
+            <button className="px-5 py-3 bg-amber-400 mr-5 rounded-lg font-bold">
+              Projects
+            </button>
+            <button className="px-5 py-3 outline rounded-lg font-bold">
+              LinkedIn
+            </button>
+          </div>
+          <div className="absolute right-[-200px] top-[-40px] z-[-1]">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={800}
+              height={629}
+              src="/image.png"
+              alt="Picture of the author"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-5xl font-black text-center decoration-amber-400 underline underline-offset-[16px] mb-[100px]">
+            Projects
+          </h1>
+          <div className="flex flex-col gap-[75px] items-center justify-center w-3/4 mx-auto">
+            <div className="rounded-lg shadow-xl bg-white flex items-center flex-nowrap">
+              <div className="flex-1 w-1/2 p-[36px]">
+                <h1 className="text-5xl font-bold mb-[32px]">Project Name</h1>
+                <p className="text-slate-500 font-light text-xl text-justify mb-[24px]">
+                  I created this personal project in order to show how to create
+                  an interface in Figma using a portfolio as an example.
+                </p>
+                <button className="px-5 py-3 outline mr-5 rounded-[24px] font-bold">
+                  View Project
+                </button>
+              </div>
+              <Image
+                className="flex-1 w-1/2 rounded-r-lg"
+                width={150}
+                height={150}
+                src="/project.png"
+                alt="Picture of the author"
+              />
+            </div>
+            <div className="rounded-lg shadow-xl bg-white flex items-center flex-nowrap">
+              <Image
+                className="flex-1 w-1/2 rounded-l-lg"
+                width={150}
+                height={150}
+                src="/project.png"
+                alt="Picture of the author"
+              />
+              <div className="flex-1 w-1/2 p-[36px]">
+                <h1 className="text-5xl font-bold mb-[32px]">Project Name</h1>
+                <p className="text-slate-500 font-light text-xl text-justify mb-[24px]">
+                  I created this personal project in order to show how to create
+                  an interface in Figma using a portfolio as an example.
+                </p>
+                <button className="px-5 py-3 outline mr-5 rounded-[24px] font-bold">
+                  View Project
+                </button>
+              </div>
+            </div>{" "}
+            <div className="rounded-lg shadow-xl bg-white flex items-center flex-nowrap">
+              <div className="flex-1 w-1/2 p-[36px]">
+                <h1 className="text-5xl font-bold mb-[32px]">Project Name</h1>
+                <p className="text-slate-500 font-light text-xl text-justify mb-[24px]">
+                  I created this personal project in order to show how to create
+                  an interface in Figma using a portfolio as an example.
+                </p>
+                <button className="px-5 py-3 outline mr-5 rounded-[24px] font-bold">
+                  View Project
+                </button>
+              </div>
+              <Image
+                className="flex-1 w-1/2 rounded-r-lg"
+                width={150}
+                height={150}
+                src="/project.png"
+                alt="Picture of the author"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-[200px]">
+          <div className="flex justify-center gap-5">
+            <AiFillInstagram size={45} />
+            <AiFillLinkedin size={45} />
+            <MdEmail size={45} />
+          </div>
+          <p className="text-center mt-5 font-light">Madelyn Torff 2021</p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
